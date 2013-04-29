7a19c08d23434af19a0a3c9737f1edd56357e333
Douglas Duteil <douglasduteil@gmail.com>
2013-04-29 03:38:00 +0200

    [FT] Doc Test 2

--- doc/doc.js
+++ doc/doc.js
@@ -1,5 +1,54 @@
-angular.module('doc.codeMirror', ['ui.codemirror'])
-  .controller('DocCtrl', ['$scope', function($scope){
-    $scope.codeMirrorModel = "Hello";
-  }])
-;
+define(['module', 'root', 'requireCss'], function (module, root, requireCss) {
+
+  var _, dfd, e$;
+
+  _ = root[module.id] || "";
+  // Hide jquery dependence...
+  dfd = new $.Deferred();
+
+
+  requireCss("http://codemirror.net/lib/codemirror.css");
+  requireCss("http://codemirror.net/theme/rubyblue.css");
+
+  requirejs(
+    {
+      paths: {
+        'ui.codemirror': _ + 'ui-codemirror'
+      },
+      shim: {
+        'ui.codemirror': { deps: ['http://codemirror.net/lib/codemirror.js'] }
+      }
+    },
+    ['ui.codemirror'],
+    function () {
+
+      angular.module('doc.ui-codeMirror', ['ui.codemirror', 'prettifyDirective' ])
+        .controller('DocCtrl', ['$scope', function ($scope) {
+          $scope.codeMirrorModel = "CodeMirror Hello World";
+          $scope.codeMirrorRefresh = false;
+
+          // Fix codeMirror display
+          // When loading codeMirror is hidden so it width is initialize at ~0...
+          $scope.$root.$watch('isLoading', function (val) {
+            // Let the time to display all the page.
+            setTimeout(function () {
+              $scope.codeMirrorRefresh = true;
+              $scope.$apply();
+            }, 2000);
+          });
+        }])
+      ;
+
+      e$ = $("#directives-ui-codemirror");
+      e$.removeAttr("ng-non-bindable");
+
+      angular.bootstrap(e$[0], ['doc.ui-codeMirror']);
+
+      dfd.resolve();
+
+    }
+  );
+
+  return dfd.promise();
+
+});
