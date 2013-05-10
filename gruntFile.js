36ae1346ff5912e13f5a4bf9f5e5bb65efe99861
Douglas Duteil <douglasduteil@gmail.com>
2013-05-10 14:51:34 +0200

    [FT] Update grunt and the demo file to make it work !

--- gruntFile.js
+++ gruntFile.js
@@ -2,9 +2,13 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'karma']);
+
+  grunt.registerTask('build-doc', ['uglify', 'copy']);
 
   var testConfig = function(configFile, customOptions) {
     var options = { configFile: configFile };
@@ -16,6 +20,20 @@ module.exports = function (grunt) {
   grunt.initConfig({
     karma: {
       unit: testConfig('test/karma.conf.js')
+    },
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: ['/**',
+        ' * <%= pkg.name %> - <%= pkg.description %>',
+        ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
+        ' * @link <%= pkg.homepage %>',
+        ' * @license <%= pkg.license %>',
+        ' */',
+        ''].join('\n'),
+      view : {
+        humaName : "UI CodeMirror",
+        repoName : "ui-codemirror"
+      }
     },
     jshint:{
       all:['ui-codemirror.js', 'gruntFile.js','test/**/*.js'],
@@ -30,6 +48,29 @@ module.exports = function (grunt) {
         boss:true,
         eqnull:true,
         globals:{}
+      }
+    },
+    uglify: {
+      options: {banner: '<%= meta.banner %>'},
+      build: {
+        files: {
+          'out/build/<%= meta.view.repoName %>.min.js': ['<%= meta.view.repoName %>.js']
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['demo/demo.html'], dest: 'out/demos.html', filter: 'isFile'}
+        ]
+      },
+      template : {
+        options : {processContent : (function(content){
+          return grunt.template.process(content);
+        })},
+        files: [
+          {src: ['out/.tmpl/index.tmpl'], dest: 'out/index.html'}
+        ]
       }
     }
   });
