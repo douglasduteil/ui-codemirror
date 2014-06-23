49de6e06daa43957897ad5e66e3e94eaddfa8164
Douglas Duteil <douglasduteil@gmail.com>
2014-06-23 10:49:48 +0200

    chore: move codemirror creation in prelink

--- src/ui-codemirror.js
+++ src/ui-codemirror.js
@@ -11,106 +11,104 @@ angular.module('ui.codemirror', [])
       restrict: 'EA',
       require: '?ngModel',
       priority: 1,
-      compile: function compile(tElement) {
+      compile: function compile() {
 
-        // Require CodeMirror
-        if (angular.isUndefined(window.CodeMirror)) {
-          throw new Error('ui-codemirror need CodeMirror to work... (o rly?)');
-        }
+        return {
+          pre: function preLink(scope, iElement) {
 
-        // Create a codemirror instance with
-        // - the function that will to place the editor into the document.
-        // - the initial content of the editor.
-        //   see http://codemirror.net/doc/manual.html#api_constructor
-        var value = tElement.text();
-        var codeMirror = new window.CodeMirror(function (cm_el) {
-
-          angular.forEach(tElement.prop('attributes'), function (a) {
-            if (a.name === 'ui-codemirror') {
-              cm_el.setAttribute('ui-codemirror-opts', a.textContent);
-            } else {
-              cm_el.setAttribute(a.name, a.textContent);
+            // Require CodeMirror
+            if (angular.isUndefined(window.CodeMirror)) {
+              throw new Error('ui-codemirror need CodeMirror to work... (o rly?)');
             }
-          });
 
-          // FIX replaceWith throw not parent Error !
-          if (tElement.parent().length <= 0) {
-            tElement.wrap('<div>');
-          }
+            var value = iElement.text();
+            iElement.html('');
 
-          tElement.replaceWith(cm_el);
-        }, {value: value});
+            // Create a codemirror instance with
+            // - the function that will to place the editor into the document.
+            // - the initial content of the editor.
+            //   see http://codemirror.net/doc/manual.html#api_constructor
+            /*jshint unused:false */
+            var _codeMirror = new window.CodeMirror(function (cm_el) {
+              iElement.append(cm_el);
+            }, {
+              value: value
+            });
 
-        return  function postLink(scope, iElement, iAttrs, ngModel) {
+          },
 
-          var options, opts;
+          post: function postLink(scope, iElement, iAttrs, ngModel) {
 
-          options = uiCodemirrorConfig.codemirror || {};
-          opts = angular.extend({}, options, scope.$eval(iAttrs.uiCodemirror), scope.$eval(iAttrs.uiCodemirrorOpts));
+            var options, opts, codeMirror;
 
-          function updateOptions(newValues) {
-            for (var key in newValues) {
-              if (newValues.hasOwnProperty(key)) {
-                codeMirror.setOption(key, newValues[key]);
+            codeMirror = iElement.children()[0].CodeMirror;
+
+            options = uiCodemirrorConfig.codemirror || {};
+            opts = angular.extend({}, options, scope.$eval(iAttrs.uiCodemirror), scope.$eval(iAttrs.uiCodemirrorOpts));
+
+            function updateOptions(newValues) {
+              for (var key in newValues) {
+                if (newValues.hasOwnProperty(key)) {
+                  codeMirror.setOption(key, newValues[key]);
+                }
               }
             }
-          }
 
-          updateOptions(opts);
+            updateOptions(opts);
 
-          if (angular.isDefined(scope.$eval(iAttrs.uiCodemirror))) {
-            scope.$watch(iAttrs.uiCodemirror, updateOptions, true);
-          }
-          // Specialize change event
-          codeMirror.on('change', function (instance) {
-            var newValue = instance.getValue();
-            if (ngModel && newValue !== ngModel.$viewValue) {
-              ngModel.$setViewValue(newValue);
+            if (angular.isDefined(scope[iAttrs.uiCodemirror])) {
+              scope.$watch(iAttrs.uiCodemirror, updateOptions, true);
             }
-            if (!scope.$$phase) {
-              scope.$apply();
-            }
-          });
-
-
-          if (ngModel) {
-            // CodeMirror expects a string, so make sure it gets one.
-            // This does not change the model.
-            ngModel.$formatters.push(function (value) {
-              if (angular.isUndefined(value) || value === null) {
-                return '';
+            // Specialize change event
+            codeMirror.on('change', function (instance) {
+              var newValue = instance.getValue();
+              if (ngModel && newValue !== ngModel.$viewValue) {
+                ngModel.$setViewValue(newValue);
               }
-              else if (angular.isObject(value) || angular.isArray(value)) {
-                throw new Error('ui-codemirror cannot use an object or an array as a model');
+              if (!scope.$$phase) {
+                scope.$apply();
               }
-              return value;
             });
 
 
-            // Override the ngModelController $render method, which is what gets called when the model is updated.
-            // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
-            ngModel.$render = function () {
-              //Code mirror expects a string so make sure it gets one
-              //Although the formatter have already done this, it can be possible that another formatter returns undefined (for example the required directive)
-              var safeViewValue = ngModel.$viewValue || '';
-              codeMirror.setValue(safeViewValue);
-            };
-          }
+            if (ngModel) {
+              // CodeMirror expects a string, so make sure it gets one.
+              // This does not change the model.
+              ngModel.$formatters.push(function (value) {
+                if (angular.isUndefined(value) || value === null) {
+                  return '';
+                } else if (angular.isObject(value) || angular.isArray(value)) {
+                  throw new Error('ui-codemirror cannot use an object or an array as a model');
+                }
+                return value;
+              });
 
 
-          // Watch ui-refresh and refresh the directive
-          if (iAttrs.uiRefresh) {
-            scope.$watch(iAttrs.uiRefresh, function (newVal, oldVal) {
-              // Skip the initial watch firing
-              if (newVal !== oldVal) {
+              // Override the ngModelController $render method, which is what gets called when the model is updated.
+              // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
+              ngModel.$render = function () {
+                //Code mirror expects a string so make sure it gets one
+                //Although the formatter have already done this, it can be possible that another formatter returns undefined (for example the required directive)
+                var safeViewValue = ngModel.$viewValue || '';
+                codeMirror.setValue(safeViewValue);
+              };
+            }
+
+
+            // Watch ui-refresh and refresh the directive
+            if (iAttrs.uiRefresh) {
+              scope.$watch(iAttrs.uiRefresh, function (newVal, oldVal) {
+                // Skip the initial watch firing
+                if (newVal !== oldVal) {
                   codeMirror.refresh();
-              }
-            });
-          }
+                }
+              });
+            }
 
-          // onLoad callback
-          if (angular.isFunction(opts.onLoad)) {
-            opts.onLoad(codeMirror);
+            // onLoad callback
+            if (angular.isFunction(opts.onLoad)) {
+              opts.onLoad(codeMirror);
+            }
           }
         };
       }
